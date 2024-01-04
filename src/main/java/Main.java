58d3996c2dcf3ebb9595a9a2da21cd836087be23
yakov23 <yakovoberderfer13@gmail.com>
2024-01-04 15:59:05 +0300

    error fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
         post.patronymic = "Иванович";
         post.birthday = date;
         post.subscription = true;
-        post.passport = "4444 № 44444444"
-        post.phone =  "+7 (999)-999-99-99"
+        post.passport = "4444 № 44444444";
+        post.phone =  "+7 (999)-999-99-99";
     }
 }
